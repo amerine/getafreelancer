dc4436daca7c9d5c35dc540773b8a11e816b0eee
Mark Turner <mark@amerine.net>
2009-10-17 02:30:28 -0700

    Tweaked README

--- test/getafreelancer_test.rb
+++ test/getafreelancer_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class GetafreelancerTest < Test::Unit::TestCase
   context "Fetching User Data" do
     setup do
-      FakeWeb.register_uri(:get, "http://api.getafreelancer.com/User/Properties.json?id=amerine&", :body => File.read("test/fixtures/user.json"))
+      FakeWeb.register_uri(:get, "http://api.getafreelancer.com/User/Properties.json?id=amerine", :body => File.read("test/fixtures/user.json"))
       @client = GetAFreelancer::Client.new
       @user = @client.user('amerine')
     end
@@ -12,8 +12,16 @@ class GetafreelancerTest < Test::Unit::TestCase
       @user.id.should == 1246655
     end
     
+    should "have a url" do
+      @user.url.should == "http://www.getafreelancer.com/users/1246655.html"
+    end
+    
     should "have a username" do
       @user.username.should == 'amerine'
+    end
+    
+    should "have a company" do 
+      @user.company.should == 'nVariable Inc.'
     end
   end
 end
